621dabe88a893cb16a4a0f1ce9a2d4b79ef483ec
JOE MON <56827756+JOEMN96@users.noreply.github.com>
2021-10-10 22:54:59 +0530

    progress

--- app.js
+++ app.js
@@ -64,3 +64,71 @@ images.forEach((img) => {
     e.preventDefault();
   });
 });
+
+// Get all sections that have an ID defined
+// const sections = document.querySelectorAll('section[id]');
+
+// // Add an event listener listening for scroll
+// window.addEventListener('scroll', navHighlighter);
+
+// function navHighlighter() {
+//   let scrollY = window.pageYOffset;
+
+//   sections.forEach((current) => {
+//     const sectionHeight = current.offsetHeight;
+//     const sectionTop = current.offsetTop - 200;
+//     let sectionId = current.getAttribute('id');
+
+//     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+//       sections.forEach((item) => {
+//         document.querySelector('.collapse.navbar-collapse a[href*=' + sectionId + ']')?.classList.add('active');
+//         if (current !== item) {
+//           document
+//             .querySelector('.collapse.navbar-collapse a[href*=' + item.getAttribute('id') + ']')
+//             ?.classList.remove('active');
+//         }
+//       });
+//     } else {
+//       // document.querySelector('.collapse.navbar-collapse a[href*=' + sectionId + ']')?.classList.remove('active');
+//     }
+//   });
+// }
+
+// Get all sections that have an ID defined
+const sections = document.querySelectorAll('section[id]');
+let navItems = document.querySelectorAll('.navbar-nav a ');
+console.log(navItems);
+// Add an event listener listening for scroll
+window.addEventListener('scroll', navHighlighter);
+
+function navHighlighter() {
+  // Get current scroll position
+  let scrollY = window.pageYOffset;
+
+  // Now we loop through sections to get height, top and ID values for each
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    let sectionId = current.getAttribute('id');
+    if (!sectionId) return;
+    /*
+    - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
+    - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
+    */
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      navItems.forEach((item) => {
+        let current = item.getAttribute('href').replace('#', '');
+        if (current == sectionId) {
+          console.log('yes');
+        } else {
+          console.log('no');
+          item.classList.remove('.active');
+        }
+      });
+      document.querySelector('.navbar-nav a[href*=' + sectionId + ']').classList.add('active');
+    } else {
+      // document.querySelector('.navigation a[href*=' + sectionId + ']').classList.remove('active');
+      return;
+    }
+  });
+}
